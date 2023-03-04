930f18acabd1a13e00d1d369cb34789363cd8fe4
Matheus Raniery <matheuscirne199@gmail.com>
2023-03-04 19:11:53 -0300

    Atualização de comentários e da aula2_print

--- aula2_print.py
+++ aula2_print.py
@@ -1,3 +1,6 @@
-#
-print(12, 34, sep='-')
-print(56, 78, sep='-')
+# O CRLF é uma quebra de linha específica do windows.Chamado de \r\n -> CRLF
+# O sep é utilizado como separador por exemplo: se estivesse sep=',' ele entederia 1,2,3 como está sep='-' ele entende 1-2-3.
+# O end é utilizado par definir o final de linha.
+print(1,2, 3,4, sep='-', end='\n')
+print(5,6, 7,8, sep='-', end='\n')
+print(9,10, 11,12, sep='-', end='\n')
