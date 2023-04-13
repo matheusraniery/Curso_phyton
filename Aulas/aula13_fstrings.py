54476162bd43d39c37bf718e15a60f83cf84e48a
Matheus Raniery <matheuscirne199@gmail.com>
2023-04-13 15:08:33 -0300

    aula14_format

--- Aulas/aula13_fstrings.py
+++ Aulas/aula13_fstrings.py
@@ -5,7 +5,7 @@ altura = float(input('Digite a altura:'))
 peso = float(input('Digite o peso:'))
 imc = peso / altura ** 2
 
-linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
 linha_2 = f'pesa {peso:.2f} quilos e seu imc é {imc:.2f}'
 linha_3 = f'{imc:.2f}'
 
